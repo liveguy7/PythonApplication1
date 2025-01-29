d2fcaf6f42c44ab82fa287be815c2fd7dfe3fb7f
jerom <jerom@LAPTOP-L0BU5SNG>
2025-01-29 15:14:52 -0500

    c3

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,11 +1,20 @@
-import sys
+import time
+from tkinter import END
 
-n = input("Enter number: ")
-n = int(n)
+def main():
+    starttime = time.time()
+    s = 0
+    for i in range(0,n+1,1):
+        s = s + i
+    end_time = time.time()
+    return s, end_time-starttime
 
-sum = (n * (n + 1)) / 2
+n = 5
+print(main())
 
-print(sum)
+
+
+
 
 
 
