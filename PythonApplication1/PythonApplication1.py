f4da302e0a92c626d400c2e8e8d7b3f7e2d4c55d
jerom <jerom@LAPTOP-L0BU5SNG>
2025-01-29 15:10:50 -0500

    c1

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,3 +1,21 @@
 import sys
 
-print("Jello")
+num = input('How many number? ')
+num = int(num)
+total_sum = 0
+
+for n in range(num):
+    numbers = float(input("Enter any number "))
+    total_sum  = total_sum + numbers
+
+
+avg = total_sum / num
+print("The average is {0}".format(avg))
+
+
+
+
+
+
+
+
