60caa1341d926de7c6e86123f8e735aefef1da6e
jerom <jerom@LAPTOP-L0BU5SNG>
2025-01-29 15:15:43 -0500

    getuser

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,16 +1,7 @@
-import time
-from tkinter import END
+import getpass
 
-def main():
-    starttime = time.time()
-    s = 0
-    for i in range(0,n+1,1):
-        s = s + i
-    end_time = time.time()
-    return s, end_time-starttime
+print(getpass.getuser())
 
-n = 5
-print(main())
 
 
 
