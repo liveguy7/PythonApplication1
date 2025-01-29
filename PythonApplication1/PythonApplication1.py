92bd4693304b80ff2945a2370fca30a4dc3e8ea0
jerom <jerom@LAPTOP-L0BU5SNG>
2025-01-29 16:28:11 -0500

    module9-10

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -3,8 +3,7 @@ import sys
 from module1 import averageNumbers
 from module2 import funcmodule2
 from module3 import methodmodule3
-from module4 import username
-from module8 import main8
+from module9 import main9
 
 
 
---
+++ PythonApplication1/module9.py
@@ -0,0 +1,15 @@
+from math import pi
+
+#find the area of a circle
+
+def main9():
+    r = input("Enter the Radius: ")
+    r = float(r)
+    area = pi * (r ** 2)
+    print("The area is {0}".format(area))
+
+
+main9()
+
+
+
